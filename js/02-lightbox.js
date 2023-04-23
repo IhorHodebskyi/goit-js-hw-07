bda56bcdf64d2bfe468c4616cb426fbdc24266ab
IhorHodebskyi <121040851+IhorHodebskyi@users.noreply.github.com>
2023-04-23 20:03:02 +0300

    task

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,34 @@ import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+console.log(galleryItems);
+const list = document.querySelector(".gallery");
+
+const cardsMarkup = createImg(galleryItems);
+list.insertAdjacentHTML("beforeend", cardsMarkup);
+
+function createImg(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+        <li class="gallery__item">
+            <a class="gallery__link" href="${original}">
+           <img class="gallery__image" src="${preview}" alt="${description}" />
+            </a>
+         </li>
+      `;
+    })
+    .join("");
+}
+list.addEventListener("click", onClick);
+
+function onClick(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+  var lightbox = new SimpleLightbox(".gallery a", {
+    captionDelay: 250,
+    captionsData: "alt",
+  });
+}
